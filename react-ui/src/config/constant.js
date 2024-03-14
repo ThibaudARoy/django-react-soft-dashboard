c066815efc5cd7dbf44a3b05d863e617aaff7bf7
ThibaudARoy <thibone14@gmail.com>
2024-03-14 16:30:22 -0400

    constants

--- react-ui/src/config/constant.js
+++ react-ui/src/config/constant.js
@@ -2,6 +2,6 @@ let BACKEND_SERVER = null;
 if (process.env.REACT_APP_BACKEND_SERVER) {
   BACKEND_SERVER = process.env.REACT_APP_BACKEND_SERVER;
 } else {
-  BACKEND_SERVER = "http://localhost:5000/api/";
+  BACKEND_SERVER = "https://django-api-test-2713-85f86601-sy3y2cbo.onporter.run/api/"; //not ideal
 }
 export const API_SERVER = BACKEND_SERVER;
